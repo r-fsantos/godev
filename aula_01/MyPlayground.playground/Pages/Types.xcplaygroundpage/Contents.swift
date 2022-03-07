31f1692a20fc6cd4dbdef1420433e4e92d323078
renato <felicio95.jr@gmail.com>
2022-03-07 20:51:07 -0300

    :hammer: Adds first set of playgrounds.

--- aula_01/MyPlayground.playground/Pages/Types.xcplaygroundpage/Contents.swift
+++ aula_01/MyPlayground.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -1,7 +1,17 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var greeting = "Hello, playground"
+// tipos primitivos
+var age: Int = 30
+let name: String  = "Renato"
+var isValid: Bool = true
+
+// tipos complexos
+var button: UIButton = UIButton()
+
+// pesquisar sobre as type nottations.
+// faz diferença na prática? vez que a linguagem é type-safe e
+// faz inferência de tipos?
 
 //: [Next](@next)
--- aula_01/aula_01_02.playground/Contents.swift
+++ aula_01/aula_01_02.playground/Contents.swift
@@ -1,3 +1,54 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Operadores de Compareção
+
+let num1: Int = 10
+let num2: Int = 20
+
+let compair: Bool = num1 < num2
+print(compair)
+
+// comparação de strings!
+let firstName: String = "renato"
+let lastName: String = "junior"
+
+// strings: array de chars
+let res: Bool = firstName < lastName
+print(res)
+
+/* operadores
+
+=   atribuicaoo
+==  comparacao
+!=  diferente
+<=
+>=
+ 
+ lógicos...
+ 
+ ||  ou lógico
+ &&  e lógico
+ !   negação
+*/
+
+/*
+ expressoes
+
+ !  inversao
+ */
+
+let isHidden: Bool = true
+let isEnabled: Bool = false
+print(!isHidden)
+
+let xpto: Bool = isHidden && isEnabled
+print(xpto)
+
+/*
+ operadores de incremento. dar uma olhada no code style
+ 
+ +=
+ -=
+ ++a  ??
+
+ */
